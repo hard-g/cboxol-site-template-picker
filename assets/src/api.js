8e89e8e22b72956134c9fa20a60386053489336d
George Mamadashvili <georgemamadashvili@gmail.com>
2021-02-16 15:42:33 +0400

    Update APIs

--- assets/src/api.js
+++ assets/src/api.js
@@ -1,9 +1,13 @@
+/**
+ * Internal dependencies
+ */
 import { buildQueryString } from './util';
 
-export async function getTemplates( categroy ) {
+export async function getSiteTemplates( category ) {
 	const endpoint = 'https://mbcc.test/wp-json/wp/v2/site-templates';
 	const query = buildQueryString({
-		_fields: [ 'id', 'title', 'excerpt', 'featured_media', 'template-category', 'site_id', 'image', 'categories' ],
+		_fields: [ 'id', 'title', 'excerpt', 'featured_media', 'template_category', 'site_id', 'image', 'categories' ],
+		template_category: category
 	} );
 
 	const response = await fetch( endpoint + '?' + query )
@@ -13,8 +17,8 @@ export async function getTemplates( categroy ) {
 		throw new Error( items.message );
 	}
 
-	const total = Number( response.headers.get('X-WP-Total') );
-	const totalPages = Number( response.headers.get('X-WP-TotalPages') );
+	const total = Number( response.headers.get( 'X-WP-Total' ) );
+	const totalPages = Number( response.headers.get( 'X-WP-TotalPages' ) );
 
 	const templates = items.map( ( item ) => {
 		return {
@@ -27,24 +31,4 @@ export async function getTemplates( categroy ) {
 	} );
 
 	return { templates, total, totalPages };
-}
-
-export async function getTemplateCategories() {
-	const endpoint = 'https://mbcc.test/wp-json/wp/v2/template-category';
-	const query = buildQueryString({
-		_fields: [ 'id', 'name', 'count' ],
-		per_page: 100,
-		order: 'desc',
-		orderby: 'count',
-		hide_empty: true,
-	} );
-
-	const response = await fetch( endpoint + '?' + query )
-	const items = await response.json();
-
-	if ( ! response.ok ) {
-		throw new Error( items.message );
-	}
-
-	return items;
 }
---
+++ assets/src/index.js
@@ -0,0 +1,7 @@
+/**
+ * Internal dependencies
+ */
+import { getSiteTemplates } from './api';
+
+const templatePicker = document.querySelector('.site-template-picker');
+const templateCategories = document.querySelector('#site-template-categories');
