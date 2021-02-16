297702fbb7c8af93b7ab96796c3e609b7621b485
George Mamadashvili <georgemamadashvili@gmail.com>
2021-02-16 19:56:45 +0400

    Don't display template picker we're cloning the group

--- inc/groups.php
+++ inc/groups.php
@@ -13,6 +13,14 @@ use function CBOX\OL\SiteTemplatePicker\Functions\view;
  * @return void
  */
 function render_template_picker() {
+	$group_id = bp_get_current_group_id();
+	$is_clone = (bool) cboxol_get_clone_source_group_id( $group_id );
+
+	// Don't display template picker we're cloning the group.
+	if ( $is_clone ) {
+		return;
+	}
+
 	$categories = get_terms( [
 		'taxonomy'   => 'cboxol_template_category',
 		'number'     => 0,
